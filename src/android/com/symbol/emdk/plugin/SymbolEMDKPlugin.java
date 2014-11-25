79548fecbf5cb4a350243a805b9fb93ca87b9ffa
Deven Bhagwan Pawar <dev.pawar.edu@gmail.com>
2014-11-24 16:57:22 -0800

    updated emdk plugin

--- src/android/com/symbol/emdk/plugin/SymbolEMDKPlugin.java
+++ src/android/com/symbol/emdk/plugin/SymbolEMDKPlugin.java
@@ -1,121 +1,293 @@
 package com.symbol.emdk.plugin;
 
+import java.util.ArrayList;
+
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import android.content.Intent;
-import android.util.Log;
+import android.os.AsyncTask;
+import android.widget.Toast;
 
-import com.symbol.datawedge.BarcodeScan;
-import com.symbol.datawedge.DataWedgeIntentHandler;
-import com.symbol.datawedge.ScanCallback;
+import com.symbol.emdk.EMDKManager;
+import com.symbol.emdk.EMDKManager.EMDKListener;
+import com.symbol.emdk.EMDKManager.FEATURE_TYPE;
+import com.symbol.emdk.EMDKResults;
+import com.symbol.emdk.barcode.BarcodeManager;
+import com.symbol.emdk.barcode.BarcodeManager.DeviceIdentifier;
+import com.symbol.emdk.barcode.ScanDataCollection;
+import com.symbol.emdk.barcode.ScanDataCollection.LabelType;
+import com.symbol.emdk.barcode.ScanDataCollection.ScanData;
+import com.symbol.emdk.barcode.Scanner;
+import com.symbol.emdk.barcode.Scanner.DataListener;
+import com.symbol.emdk.barcode.Scanner.StatusListener;
+import com.symbol.emdk.barcode.Scanner.TriggerType;
+import com.symbol.emdk.barcode.ScannerException;
+import com.symbol.emdk.barcode.ScannerResults;
+import com.symbol.emdk.barcode.StatusData;
+import com.symbol.emdk.barcode.StatusData.ScannerStates;
 
-public class SymbolDataWedgePlugin extends CordovaPlugin {
-    
-    private DataWedgeIntentHandler wedge;
-    protected static String TAG = "SymbolDatawedgePlugin";
+public class SymbolEMDKPlugin extends CordovaPlugin implements EMDKListener,
+		StatusListener, DataListener {
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView)
-    {
-        super.initialize(cordova, webView);
-        wedge = new DataWedgeIntentHandler(cordova.getActivity().getBaseContext());
-    }
-    @Override
-    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if ("scanner.register".equals(action)) {
-            wedge.setScanCallback(new ScanCallback<BarcodeScan>() {
-                @Override
-                public void execute(BarcodeScan scan) {
-                    Log.i(TAG, "Scan result [" + scan.LabelType + "-" + scan.Barcode + "].");
-                    
-                    try {
-                        JSONObject obj = new JSONObject();
-                        obj.put("type", scan.LabelType);
-                        obj.put("barcode", scan.Barcode);
-                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, obj);
-                        pluginResult.setKeepCallback(true);
-                        callbackContext.sendPluginResult(pluginResult);
-                    } catch(JSONException e){
-                        Log.e(TAG, "Error building json object", e);
-                        
-                    }
-                }
-            });
-        }
-        else if ("scanner.unregister".equals(action)) {
-            wedge.setScanCallback(null);
-            if (!wedge.hasListeners()) {
-                wedge.stop();
-            }
-        }
-        else if ("scanner.softScanOn".equals(action)){
-            wedge.startScanning(true);
-            callbackContext.success();
-        }
-        else if ("scanner.softScanOff".equals(action)) {
-            wedge.startScanning(false);
-            callbackContext.success();
-        }
+	// Declare a variable to store EMDKManager object
+	private EMDKManager emdkManager = null;
 
-        //register for plugin callbacks
-        else if ("switchProfile".equals(action)){
-            wedge.switchProfile(args.getString(0));
-        }
+	// Declare a variable to store Barcode Manager object
+	private BarcodeManager barcodeManager = null;
 
-        else if ("stop".equals(action)){
-            wedge.stop();
-        }
+	// Declare a variable to hold scanner device to scan
+	private Scanner scanner = null;
 
+	@Override
+	public boolean execute(String action, JSONArray args,
+			final CallbackContext callbackContext) throws JSONException {
+		if ("scanner.softScanOn".equals(action)) {
+			// The EMDKManager object will be created and returned in the
+			// callback.
+			EMDKResults results = EMDKManager.getEMDKManager(cordova
+					.getActivity().getBaseContext(), this);
+			// Check the return status of getEMDKManager and update the status
+			// Text
+			// View accordingly
+			if (results.statusCode != EMDKResults.STATUS_CODE.SUCCESS) {
+				System.out
+						.println("*************EMDKManager Request Failed****************");
+			}
 
-        //start plugin now if not already started
-        if ("start".equals(action) || "scanner.register".equals(action)) {
+			System.out
+					.println("*************EMDKManager Request Success****************");
+		}
+		return true;
+	}
 
-            //try to read intent action from inbound params
-            String intentAction = null;
-            if (args.length() > 0) {
-                intentAction = args.getString(0);  
-            } 
-            if (intentAction != null && intentAction.length() > 0) {
-                Log.i(TAG, "Intent action length  " + intentAction.length());
+	@Override
+	public void onClosed() {
+		// TODO Auto-generated method stub
+		// The EMDK closed abruptly. // Clean up the objects created by EMDK
+		// manager
+		if (this.emdkManager != null) {
 
-                wedge.setDataWedgeIntentAction(intentAction);
-            }
-            wedge.start();
-        } 
+			this.emdkManager.release();
+			this.emdkManager = null;
+		}
+	}
 
-        return true;
-    }
-    /**
-    * Always close the current intent reader
-    */
-    @Override
-    public void onPause(boolean multitasking)
-    {
-        super.onPause(multitasking);
-        wedge.stop();
-    }
+	@Override
+	public void onOpened(EMDKManager emdkManager) {
+		// TODO Auto-generated method stub
+		this.emdkManager = emdkManager;
 
-    @Override
-    public void onNewIntent(Intent intent) {
-        
-        Log.i(TAG, "Got inbound intent  " + intent.getAction());
-        wedge.handleIntent(intent);
-    }
+		try {
+			// Call this method to enable Scanner and its listeners
+			initializeScanner();
+		} catch (ScannerException e) {
+			e.printStackTrace();
+		}
 
-    /**
-    * Always resume the current activity
-    */
-    @Override
-    public void onResume(boolean multitasking)
-    {
-        super.onResume(multitasking);
-        wedge.start();
-    }
+		// Toast to indicate that the user can now start scanning
+		Toast.makeText(cordova.getActivity().getBaseContext(),
+				"Press Hard Scan Button to start scanning...",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onData(ScanDataCollection scanDataCollection) {
+		// TODO Auto-generated method stub
+		// Use the scanned data, process it on background thread using AsyncTask
+		// and update the UI thread with the scanned results
+		new AsyncDataUpdate().execute(scanDataCollection);
+
+	}
+
+	// Update the scan data on UI
+	int dataLength = 0;
+
+	// AsyncTask that configures the scanned data on background
+	// thread and updated the result on UI thread with scanned data and type of
+	// label
+	private class AsyncDataUpdate extends
+			AsyncTask<ScanDataCollection, Void, String> {
+
+		@Override
+		protected String doInBackground(ScanDataCollection... params) {
+
+			// Status string that contains both barcode data and type of barcode
+			// that is being scanned
+			String statusStr = "";
+
+			try {
+
+				// Starts an asynchronous Scan. The method will not turn ON the
+				// scanner. It will, however, put the scanner in a state in
+				// which
+				// the scanner can be turned ON either by pressing a hardware
+				// trigger or can be turned ON automatically.
+				scanner.read();
+
+				ScanDataCollection scanDataCollection = params[0];
+
+				// The ScanDataCollection object gives scanning result and the
+				// collection of ScanData. So check the data and its status
+				if (scanDataCollection != null
+						&& scanDataCollection.getResult() == ScannerResults.SUCCESS) {
+
+					ArrayList<ScanData> scanData = scanDataCollection
+							.getScanData();
+
+					// Iterate through scanned data and prepare the statusStr
+					for (ScanData data : scanData) {
+						// Get the scanned data
+						String barcodeDate = data.getData();
+						// Get the type of label being scanned
+						LabelType labelType = data.getLabelType();
+						// Concatenate barcode data and label type
+						statusStr = barcodeDate + " " + labelType;
+					}
+				}
+
+			} catch (ScannerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// Return result to populate on UI thread
+			return statusStr;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// Update the dataView EditText on UI thread with barcode data and
+			// its label type
+
+			System.out.println("*********Barcode Data***************" + result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+		}
+	}
+
+	@Override
+	public void onStatus(StatusData statusData) {
+		// TODO Auto-generated method stub
+		// process the scan status event on the background thread using
+		// AsyncTask and update the UI thread with current scanner state
+		new AsyncStatusUpdate().execute(statusData);
+
+	}
+
+	// AsyncTask that configures the current state of scanner on background
+	// thread and updates the result on UI thread
+	private class AsyncStatusUpdate extends AsyncTask<StatusData, Void, String> {
+
+		@Override
+		protected String doInBackground(StatusData... params) {
+			String statusStr = "";
+			// Get the current state of scanner in background
+			StatusData statusData = params[0];
+			ScannerStates state = statusData.getState();
+			// Different states of Scanner
+			switch (state) {
+			// Scanner is IDLE
+			case IDLE:
+				statusStr = "The scanner enabled and its idle";
+				break;
+			// Scanner is SCANNING
+			case SCANNING:
+				statusStr = "Scanning..";
+				break;
+			// Scanner is waiting for trigger press
+			case WAITING:
+				statusStr = "Waiting for trigger press..";
+				break;
+			// Scanner is not enabled
+			case DISABLED:
+				statusStr = "Scanner is not enabled";
+				break;
+			default:
+				break;
+			}
+
+			// Return result to populate on UI thread
+			return statusStr;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// Update the status text view on UI thread with current scanner
+			// state
+			System.out.println("****************Barcode Status***************"
+					+ result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+		}
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		try {
+			if (scanner != null) {
+				// releases the scanner hardware resources for other application
+				// to use. You must call this as soon as you're done with the
+				// scanning.
+				scanner.disable();
+				scanner = null;
+			}
+
+			if (emdkManager != null) {
+
+				// Clean up the objects created by EMDK manager
+				emdkManager.release();
+				emdkManager = null;
+			}
+
+		} catch (ScannerException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Method to initialize and enable Scanner and its listeners
+	private void initializeScanner() throws ScannerException {
+
+		if (scanner == null) {
+
+			// Get the Barcode Manager object
+			barcodeManager = (BarcodeManager) this.emdkManager
+					.getInstance(FEATURE_TYPE.BARCODE);
+
+			// Get default scanner defined on the device
+			scanner = barcodeManager.getDevice(DeviceIdentifier.DEFAULT);
+
+			// Add data and status listeners
+			scanner.addDataListener(this);
+			scanner.addStatusListener(this);
+
+			// Hard trigger. When this mode is set, the user has to manually
+			// press the trigger on the device after issuing the read call.
+			scanner.triggerType = TriggerType.HARD;
+
+			// Enable the scanner
+			scanner.enable();
+
+			// Starts an asynchronous Scan. The method will not turn ON the
+			// scanner. It will, however, put the scanner in a state in which
+			// the scanner can be turned ON either by pressing a hardware
+			// trigger or can be turned ON automatically.
+			scanner.read();
+		}
+	}
+
 }
--- www/emdk.js
+++ www/emdk.js
@@ -4,81 +4,24 @@ var exec = require('cordova/exec');
  /**
          * Constructor.
  *
- * @returns {DataWedge}
+ * @returns {SymbolEMDK}
  */
-function DataWedge() {
+function SymbolEMDK() {
 
 };
 
-/**
- * Turn on DataWedge (default profile) and listen for event.  Listens for hardward button events.
- * 
- * @param successCallback - Success function should expect a barcode to be passed in
- * @param intentAction - action to listen for.  This is what you configured in the DataWedge app.  
- *      
- */
-DataWedge.prototype.start = function (intentAction) {
-    var args = [];
-    if (intentAction) {
-        args[0] = intentAction;
-    }
-    exec(null, null, 'SymbolDataWedge', 'start', args);
-};
-/**
- * Turn off DataWedge plugin
- */
-DataWedge.prototype.stop = function () {
-  
-    exec(null, null, 'SymbolDataWedge', 'stop', []);
-};
-
-/**
- * Activate a different profile for the data wedge.  For instance, to enable data processing rules
- */
-DataWedge.prototype.switchProfile = function (profileName) {
-    if (!profileName)  {
-        console.log("DataWedge.switchProfile did not include a profile.  A profile name is required.");
-        return;
-    }
-    exec(null, null, 'SymbolDataWedge', 'switchProfile', [profileName]);
-};
-
-
-/**
- * Register a callback for scan events.  This function will be called when barcdoes are read
- */
-DataWedge.prototype.registerForBarcode = function (callback) {
-    
-    exec(callback, null, 'SymbolDataWedge', 'scanner.register', []);
-};
-
-/**
- * De-register a callback for scan events.  
- */
-DataWedge.prototype.unregisterBarcode = function () {
-    
-    exec(null, null, 'SymbolDataWedge', 'scanner.unregister', []);
-};
 
 /**
  * Manually turn on barcode scanner
  */
-DataWedge.prototype.startScanner = function () {
+SymbolEMDK.prototype.startScanner = function () {
     
-    exec(null, null, 'SymbolDataWedge', 'scanner.softScanOn', []);
+    exec(null, null, 'SymbolEMDK', 'scanner.softScanOn', []);
 };
-
-/**
- * Manually turn off barcode scanner
- */
-DataWedge.prototype.stopScanner = function () {
-    exec(null, null, 'SymbolDataWedge', 'scanner.softScanOff', []);
-};
-
 
 
 
 //create instance
-var DataWedge = new DataWedge();
+var SymbolEMDK = new SymbolEMDK();
 
-module.exports = DataWedge;
+module.exports = SymbolEMDK;
